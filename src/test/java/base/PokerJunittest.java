34f8cc23f7d5578a2728368691574e0dae831780
Mark Seda <markpseda@gmail.com>
2016-03-02 09:11:17 -0500

    added most classes

--- src/test/java/base/PokerJunittest.java
+++ src/test/java/base/PokerJunittest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class MyInteger_Test {
+public class PokerJunittest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
