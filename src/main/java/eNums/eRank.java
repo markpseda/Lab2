81767f653efb2adad4adda99973cbcf6e08279d1
Mark Seda <markpseda@gmail.com>
2016-03-02 09:22:44 -0500

    eRank Complete

--- src/main/java/eNums/eRank.java
+++ src/main/java/eNums/eRank.java
@@ -1,5 +1,30 @@
 package eNums;
 
 public enum eRank {
+	
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9),
+	TEN(10),
+	JACK(11),
+	QUEEN(12),
+	KING(13),
+	ACE(14);
+	
+	private int iRank;
+	
+	private eRank(int iRank){
+		this.iRank = iRank;
+	}
+
+	public int getiRank() {
+		return iRank;
+	}
+	
 
 }
--- src/main/java/eNums/eSuit.java
+++ src/main/java/eNums/eSuit.java
@@ -1,5 +1,12 @@
 package eNums;
 
 public enum eSuit {
-
+	SPADES(2);
+	
+	private int iSuit;
+	
+	private eSuit(int iSuit){
+		this.iSuit = iSuit;
+	}
+	
 }
